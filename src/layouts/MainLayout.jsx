5eafa3041df3f264316c678eff7b1aba0fa978c4
ashikur <ashikur1602@gmail.com>
2024-07-31 16:48:51 +0600

    working on footer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../components/frontendComponents/Footer';
 
 const MainLayout = () => {
     return (
         <>
             <Outlet></Outlet>
+            <Footer></Footer>
         </>
     );
 };
