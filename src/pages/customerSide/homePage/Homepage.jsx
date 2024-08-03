a193ebd0585089040b59f696a574c507ffdaaeda
ashikur <ashikur1602@gmail.com>
2024-08-03 15:08:46 +0600

    working on footer

--- src/pages/customerSide/homePage/Homepage.jsx
+++ src/pages/customerSide/homePage/Homepage.jsx
@@ -6,7 +6,7 @@ const Homepage = () => {
     return (
         <div>
             I am from homepage components
-            <img src="https://res.cloudinary.com/deifi77os/image/upload/v1722300206/tndtjfvifekxgkrwcscn.jpg" alt="" />
+            
         </div>
     );
 };
