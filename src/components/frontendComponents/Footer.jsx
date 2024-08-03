f86d9b5140a4b36242a6072f9cd18372fae76224
ashiku2 <ashikur1602@gmail.com>
2024-08-03 22:29:05 +0600

    Footer design completed

--- src/components/frontendComponents/Footer.jsx
+++ src/components/frontendComponents/Footer.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { BiLogoMastercard } from 'react-icons/bi';
+import { FaCcDiscover, FaCcMastercard, FaPaypal } from 'react-icons/fa';
+import { RiBankCard2Line, RiMastercardLine, RiVisaFill } from 'react-icons/ri';
+import { SiAmericanexpress, SiBitcoin } from 'react-icons/si';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
@@ -6,7 +10,7 @@ const Footer = () => {
         <>
             <footer className="bg-blue-600 text-white">
                 <div className='flex flex-col lg:flex-row justify-between w-1/2  lg:w-10/12 mx-auto  py-10'>
-                    
+
                     <div className='flex flex-col basis-1/4 space-y-2'>
                         <p className='text-2xl'>Discover</p>
                         <p>Home</p>
@@ -18,68 +22,69 @@ const Footer = () => {
 
                     </div>
 
-                    <div className='flex flex-col basis-1/4 space-y-2'>
-                        <p className='text-2xl'>Payment Methods</p>
-                        <p>Home</p>
-                        <p>Terms & conditions</p>
-                        <p>Career</p>
-                        <p>Refund Policy</p>
-                        <p>EMI Policy</p>
-                        <p>Privacy Policy</p>
+                    <div className='flex flex-col basis-1/4 space-y-2 '>
+                        <p className='text-2xl mb-5'>Payment Methods</p>
+                        {/* <div className='text-4xl space-y-5 '>
+                            <div className='flex gap-8'>
+                                <p className=''><RiVisaFill /></p>
+                                <p className=''><SiAmericanexpress /></p>
+                                <p><FaCcMastercard /></p>
+                            </div>
+                            <div className="flex  gap-8">
+                                <p className=''><FaPaypal /></p>
+                                <p><RiMastercardLine /></p>
+                                <p><RiBankCard2Line /></p>
+                            </div>
+                            <div className="flex gap-8">
+                                <p><BiLogoMastercard /></p>
+                                <p><FaCcDiscover /></p>
+                                <p><SiBitcoin /></p>
+                            </div>
+                        </div> */}
 
+                        <div className='lg:ml-6'>
+                            <img src="https://res.cloudinary.com/deifi77os/image/upload/v1722700494/Bideshgami/scvpwmf9pexosg2ikfvy.png" alt="" />
+                        </div>
                     </div>
 
                     <div className='flex flex-col basis-1/4 space-y-2'>
                         <p className='text-2xl'>Need Help ?</p>
                         <p>We are Always here for you! <br /> Knock us on Messanger anytime <br /> or Call our Hotline (10AM - 10PM).</p>
-                        
+
                         <p className='text-2xl mt-20'>Contact</p>
                         <p>info@bideshgami.com</p>
                         <p>+8845345343345</p>
                     </div>
 
                     <div className='flex flex-col basis-1/4 space-y-2'>
-                        <p className='text-2xl'>Download our app</p>
-                        <p>Home</p>
-                        <p>Terms & conditions</p>
-                        <p>Career</p>
-                        <p>Refund Policy</p>
-                        <p>EMI Policy</p>
-                        <p>Privacy Policy</p>
+                        <div className='text-center'>
+                            <p className='text-2xl'>Download our app</p>
+                            <div className='flex justify-center'>
+                                <img src="https://res.cloudinary.com/deifi77os/image/upload/v1722700494/Bideshgami/k9c2qx9dpap03abxm8xs.png" alt="" />
+                            </div>
+                            <p className='text-2xl'>Follow Us</p>
+                            <div className='flex justify-center'>
+                                <img src="https://res.cloudinary.com/deifi77os/image/upload/v1722700494/Bideshgami/uec8sawxfqbbgxoptckw.png" alt="" />
+                            </div>
+                        </div>
 
                     </div>
 
 
 
                 </div>
-                <div className="bg-gray-100">
+
+                <hr />
+
+                <div className="bg-blue-600 border border-black ">
                     <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-                        <p className="text-gray-500 text-sm text-center sm:text-left">© 2020 Tailblocks —
-                            <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
-                        </p>
-                        <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-                            <a className="text-gray-500">
-                                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                                </svg>
-                            </a>
-                            <a className="ml-3 text-gray-500">
-                                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                                </svg>
-                            </a>
-                            <a className="ml-3 text-gray-500">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                                    <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                                </svg>
-                            </a>
-                            <a className="ml-3 text-gray-500">
-                                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" className="w-5 h-5" viewBox="0 0 24 24">
-                                    <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
-                                    <circle cx="4" cy="4" r="2" stroke="none"></circle>
-                                </svg>
-                            </a>
+                        
+                        <img src="https://res.cloudinary.com/deifi77os/image/upload/v1722700494/Bideshgami/kbcp5nbx2ssawy4hwd1f.png" alt="" />
+
+
+                        <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center  items-center sm:justify-start">
+                            <p className="text-white  text-center sm:text-left">© Copyright Bideshgami.
+                            </p>
                         </span>
                     </div>
                 </div>
