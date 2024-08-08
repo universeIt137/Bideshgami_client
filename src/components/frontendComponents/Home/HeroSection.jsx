71180df33fcee81f17c67356367ac2978ed40912
Mobinul Islam Mahi <mobinulislammahi@gmail.com>
2024-08-08 12:33:30 +0600

    herosection image update

--- src/components/frontendComponents/Home/HeroSection.jsx
+++ src/components/frontendComponents/Home/HeroSection.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, {useEffect } from "react";
 import './HeroSection.css';
 import { MdOutlineSettingsInputComponent } from "react-icons/md";
 import { Link } from "react-router-dom";
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination } from 'swiper/modules';
 import AirTicket from '../../../assets/icon/airoplane.png';
 import honeymoon from '../../../assets/icon/honeymoon.png';
 import hotel from '../../../assets/icon/hotel.png';
@@ -13,6 +18,31 @@ import student from '../../../assets/icon/student.png';
 import workpermit from '../../../assets/icon/workpermit.png';
 
 const HeroSection = () => {
+  useEffect(() => {
+    // Initialize Swiper
+    const swiper = new Swiper('.swiper', {
+      modules: [Navigation, Pagination],
+      slidesPerView: 2, // Show 2 slides at a time
+      spaceBetween: 10, // Space between slides
+      navigation: true, // Show navigation arrows
+      pagination: { // Show pagination bullets
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 1, // Show 1 slide at a time on small screens
+        },
+        768: {
+          slidesPerView: 2, // Show 2 slides at a time on larger screens
+        },
+      },
+    });
+
+    return () => {
+      swiper.destroy(); // Clean up Swiper instance on unmount
+    };
+  }, []);
 
   let dataIcon = [
     {
@@ -125,15 +155,23 @@ const HeroSection = () => {
       </div>
       {/* Slider */}
       <div className="container mx-auto mt-5">
-        <div className='grid gap-3 grid-cols-2'>
-          <div className="">
-            <img className="rounded-md w-full" src='Component-1.png' alt="component1"/>
+      <div className="swiper">
+        <div className="swiper-wrapper">
+          <div className="swiper-slide">
+            <img className="rounded-md w-full h-auto" src='Component-1.png' alt="component1"/>
           </div>
-          <div className="">
-            <img className="rounded-md w-full" src='Component-2.png' alt="component1"/>
+          <div className="swiper-slide">
+            <img className="rounded-md w-full h-auto" src='Component-2.png' alt="component2"/>
           </div>
+          {/* Add more slides as needed */}
         </div>
+        {/* Pagination */}
+        <div className="swiper-pagination"></div>
+        {/* Navigation */}
+        <div className="swiper-button-next"></div>
+        <div className="swiper-button-prev"></div>
       </div>
+    </div>
 
       
      </>
