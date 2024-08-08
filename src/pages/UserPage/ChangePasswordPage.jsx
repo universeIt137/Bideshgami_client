706d0cdcfe36d3ae39448a4b1c7c85e50585b8f5
Mobinul Islam Mahi <mobinulislammahi@gmail.com>
2024-08-08 16:01:32 +0600

    registration page

---
+++ src/pages/UserPage/ChangePasswordPage.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import ChangePassword from "../../components/frontendComponents/UserAuth/ChangePassword";
+
+const ChangePasswordPage = () => {
+  return (
+    <div>
+      <ChangePassword/>
+    </div>
+  )
+};
+
+export default ChangePasswordPage
---
+++ src/pages/UserPage/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Login from "../../components/frontendComponents/UserAuth/Login";
+
+const LoginPage = () => {
+  return (
+    <>
+      <Login/>
+    </>
+  )
+};
+
+export default LoginPage
